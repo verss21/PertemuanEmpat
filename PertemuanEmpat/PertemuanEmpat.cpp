4ec18e19702e37898ef0d83743cdd4dc805cd0f3
verss21 <arjunafrizal01@gmail.com>
2024-03-18 11:39:30 +0700

    Add project files.

---
+++ PertemuanEmpat/PertemuanEmpat.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main();
+{
+    int i;
+
+    for (i = 0; i < 6; i++) {
+        cout << i + 1 << ". " << "nama saya adalah budi" << endl;
+    }
+
+    string arr[5];
+
+    for (i = 0; < 5: 1++) {
+        cout << "masukan nickname ke " << i = 1 << " = ";
+        cin >> arr[i];
+    }
+
+    for (i = 0; 1 < 5; i++) {
+        cout << "namanya adalah = " << arr[i] << endl;
+    }
